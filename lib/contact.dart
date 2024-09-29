160e82f208742ba6a7e9f7be6ee05fd8a5636f5c
Rilo Prianoko <prianoko11rilo@gmail.com>
2024-09-29 22:51:51 +0700

    fixing overflow

--- lib/contact.dart
+++ lib/contact.dart
@@ -13,102 +13,121 @@ class ContactMe extends StatelessWidget {
           child: Stack(
             children: [
               Column(
-                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  const CircleAvatar(
-                    radius: 50,
-                    backgroundImage: const NetworkImage(
-                      'https://res.cloudinary.com/dd4uyxyju/image/upload/v1727079232/Mask_Adult_Hero_MO_uppt3c.jpg',
-                    ),
+                  // Bagian yang tidak ikut di-scroll
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const CircleAvatar(
+                        radius: 50,
+                        backgroundImage: NetworkImage(
+                          'https://res.cloudinary.com/dd4uyxyju/image/upload/v1727079232/Mask_Adult_Hero_MO_uppt3c.jpg',
+                        ),
+                      ),
+                      Text(
+                        'Rilo Prianoko',
+                        style: GoogleFonts.pacifico(
+                          fontSize: 28,
+                          color: Colors.white54,
+                        ),
+                      ),
+                      Text(
+                        'Data Scientist',
+                        style: GoogleFonts.pirataOne(
+                          color: Colors.teal.shade100,
+                          fontSize: 20,
+                          letterSpacing: 2.5,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                        width: 80,
+                        child: Divider(
+                          color: Colors.tealAccent,
+                        ),
+                      ),
+                    ],
                   ),
-                  Text('Rilo Prianoko',
-                      style: GoogleFonts.pacifico(
-                          fontSize: 28, color: Colors.white54)),
-                  Text(
-                    'Data Scientist',
-                    style: GoogleFonts.pirataOne(
-                        color: Colors.teal.shade100,
-                        fontSize: 20,
-                        letterSpacing: 2.5,
-                        fontWeight: FontWeight.bold),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                    width: 80,
-                    child: Divider(
-                      color: Colors.tealAccent,
-                    ),
-                  ),
-                  const Card(
-                    color: Colors.white,
-                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
-                    child: const ListTile(
-                      leading: Icon(
-                        Icons.phone,
-                        color: Colors.teal,
+
+                  // Expanded agar bagian ini bisa di-scroll
+                  Expanded(
+                    child: SingleChildScrollView(
+                      child: Column(
+                        children: [
+                          const Card(
+                            color: Colors.white,
+                            margin: EdgeInsets.symmetric(
+                                vertical: 10, horizontal: 25),
+                            child: ListTile(
+                              leading: Icon(
+                                Icons.phone,
+                                color: Colors.teal,
+                              ),
+                              title: Text('+62 90242422'),
+                            ),
+                          ),
+                          Card(
+                            color: Colors.white,
+                            margin: const EdgeInsets.symmetric(
+                                vertical: 10, horizontal: 25),
+                            child: const ListTile(
+                              leading: Icon(
+                                Icons.email,
+                                color: Colors.teal,
+                              ),
+                              title: Text('prianoko11rilo@gmail.com'),
+                            ),
+                          ),
+                          Card(
+                            color: Colors.white,
+                            margin: const EdgeInsets.symmetric(
+                                vertical: 10, horizontal: 25),
+                            child: ListTile(
+                              leading: Image.network(
+                                'https://img.icons8.com/?size=100&id=13930&format=png&color=000000',
+                                width: 28,
+                                height: 28,
+                              ),
+                              title: const Text(
+                                'https://www.linkedin.com/in/riloprianoko/',
+                                style: TextStyle(fontSize: 12),
+                              ),
+                            ),
+                          ),
+                          Card(
+                            color: Colors.white,
+                            margin: const EdgeInsets.symmetric(
+                                vertical: 10, horizontal: 25),
+                            child: ListTile(
+                              leading: Image.network(
+                                'https://img.icons8.com/?size=100&id=62856&format=png&color=000000',
+                                width: 28,
+                                height: 28,
+                              ),
+                              title: const Text('https://github.com/realrilo'),
+                            ),
+                          ),
+                          const Card(
+                            color: Colors.white,
+                            margin: EdgeInsets.symmetric(
+                                vertical: 10, horizontal: 25),
+                            child: ListTile(
+                              leading: Icon(
+                                Icons.home,
+                                color: Colors.teal,
+                              ),
+                              title: Text('Jakarta, Indonesia'),
+                            ),
+                          ),
+                        ],
                       ),
-                      title: Text('+62 90242422'),
-                    ),
-                  ),
-                  Card(
-                    color: Colors.white,
-                    margin: const EdgeInsets.symmetric(
-                        vertical: 10, horizontal: 25),
-                    child: ListTile(
-                      leading: Icon(
-                        Icons.email,
-                        color: Colors.teal,
-                      ),
-                      title: Text(
-                        'prianoko11rilo@gmail.com',
-                      ),
-                    ),
-                  ),
-                  Card(
-                    color: Colors.white,
-                    margin: const EdgeInsets.symmetric(
-                        vertical: 10, horizontal: 25),
-                    child: ListTile(
-                      leading: Image.network(
-                        'https://img.icons8.com/?size=100&id=13930&format=png&color=000000',
-                        width: 28,
-                        height: 28,
-                      ),
-                      title: const Text(
-                        'https://www.linkedin.com/in/riloprianoko/',
-                        style: TextStyle(fontSize: 12),
-                      ),
-                    ),
-                  ),
-                  Card(
-                    color: Colors.white,
-                    margin: const EdgeInsets.symmetric(
-                        vertical: 10, horizontal: 25),
-                    child: ListTile(
-                      leading: Image.network(
-                        'https://img.icons8.com/?size=100&id=62856&format=png&color=000000',
-                        width: 28,
-                        height: 28,
-                      ),
-                      title: const Text(
-                        'https://github.com/realrilo',
-                      ),
-                    ),
-                  ),
-                  Card(
-                    color: Colors.white,
-                    margin: const EdgeInsets.symmetric(
-                        vertical: 10, horizontal: 25),
-                    child: const ListTile(
-                      leading: Icon(
-                        Icons.home,
-                        color: Colors.teal,
-                      ),
-                      title: Text('Jakarta, Indonesia'),
                     ),
                   ),
                 ],
               ),
+
+              // tomobl balik
               Positioned(
                 top: 10,
                 left: 10,
--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -12,19 +12,19 @@ class MainScreen extends StatelessWidget {
         title: const Text("Hi, There...I'm Rilo"),
       ),
       body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          const Card(
+          // Bagian yang tidak ikut scroll
+          Card(
             child: Row(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                CircleAvatar(
+                const CircleAvatar(
                   radius: 60,
                   backgroundImage: NetworkImage(
                     'https://res.cloudinary.com/dd4uyxyju/image/upload/v1727079232/Mask_Adult_Hero_MO_uppt3c.jpg',
                   ),
                 ),
-                const Expanded(
+                Expanded(
                   flex: 1, // Memberikan ruang proporsional untuk teks
                   child: Padding(
                     padding: const EdgeInsets.all(16.0),
@@ -50,213 +50,177 @@ class MainScreen extends StatelessWidget {
                 ),
               ],
             ),
-          ), //image
+          ), // image
+
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               OutlinedButton(
-                  onPressed: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) {
-                      return PortofolioScreen();
-                    }));
-                  },
-                  child: const Text("Click for My Portofolio")),
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) {
+                    return PortofolioScreen();
+                  }));
+                },
+                child: const Text("Click for My Portofolio"),
+              ),
               OutlinedButton(
-                  onPressed: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) {
-                      return ContactMe();
-                    }));
-                  },
-                  child: const Text("Contact Me for more")),
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) {
+                    return ContactMe();
+                  }));
+                },
+                child: const Text("Contact Me for more"),
+              ),
             ],
           ),
-          const SizedBox(
-            height: 25,
+
+          // Expanded to ensure scrolling for the rest of the content
+          Expanded(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  const SizedBox(
+                    height: 25,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Container(
+                      padding: const EdgeInsets.all(16.0),
+                      decoration: BoxDecoration(
+                        color: Colors.grey[100],
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: const Text(
+                        'I am Rilo Prianoko, a 7th-semester student at Gunadarma University. I have a strong interest in Machine Learning, Data Science, and Flutter-based application development. Besides focusing on academics, '
+                        'I also have experience working on deep learning projects for customer churn prediction and medical image classification. Currently, I am continuously developing my skills in programming and data technology to achieve a better career in the future.',
+                        style: TextStyle(fontSize: 15, color: Colors.black),
+                        textAlign: TextAlign.justify, // Mengatur text alignment
+                      ),
+                    ),
+                  ), // desc
+
+                  const SizedBox(height: 16),
+
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Container(
+                      padding: const EdgeInsets.all(16.0),
+                      decoration: BoxDecoration(
+                        color: Colors.blue[200],
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            'Tools',
+                            style: TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.bold),
+                          ),
+                          const SizedBox(
+                              height: 10), // Spasi antara judul dan icon
+
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              // Icon TensorFlow
+                              Column(
+                                children: [
+                                  Container(
+                                    width: 30,
+                                    height: 30,
+                                    child: Image.network(
+                                      'https://img.icons8.com/?size=100&id=n3QRpDA7KZ7P&format=png&color=000000',
+                                      fit: BoxFit.contain,
+                                    ),
+                                  ),
+                                  Container(
+                                    width: 30,
+                                    height: 30,
+                                    child: Image.network(
+                                      'https://img.icons8.com/?size=100&id=13441&format=png&color=000000',
+                                    ),
+                                  ),
+                                  Container(
+                                    width: 30,
+                                    height: 30,
+                                    child: Image.network(
+                                      'https://img.icons8.com/?size=100&id=lOqoeP2Zy02f&format=png&color=000000',
+                                      fit: BoxFit.contain,
+                                    ),
+                                  ),
+                                ],
+                              ),
+
+                              // Icon Power BI
+                              Column(
+                                children: [
+                                  Container(
+                                    width: 30,
+                                    height: 30,
+                                    child: Image.network(
+                                      'https://img.icons8.com/?size=100&id=qYfwpsRXEcpc&format=png&color=000000',
+                                      fit: BoxFit.contain,
+                                    ),
+                                  ),
+                                  Container(
+                                    width: 30,
+                                    height: 30,
+                                    child: Image.network(
+                                      'https://img.icons8.com/?size=100&id=55251&format=png&color=000000',
+                                      fit: BoxFit.contain,
+                                    ),
+                                  ),
+                                  Container(
+                                    width: 30,
+                                    height: 30,
+                                    child: Image.network(
+                                      'https://img.icons8.com/?size=100&id=9Kvi1p1F0tUo&format=png&color=000000',
+                                      fit: BoxFit.contain,
+                                    ),
+                                  ),
+                                ],
+                              ),
+
+                              // Icon Jupyter
+                              Column(
+                                children: [
+                                  Container(
+                                    width: 30,
+                                    height: 30,
+                                    child: Image.network(
+                                      'https://img.icons8.com/?size=100&id=aR9CXyMagKIS&format=png&color=000000',
+                                      fit: BoxFit.contain,
+                                    ),
+                                  ),
+                                  Container(
+                                    width: 30,
+                                    height: 30,
+                                    child: Image.network(
+                                      'https://img.icons8.com/?size=100&id=SruJhzn0nnLl&format=png&color=000000',
+                                      fit: BoxFit.contain,
+                                    ),
+                                  ),
+                                  Container(
+                                    width: 30,
+                                    height: 30,
+                                    child: Image.network(
+                                      'https://img.icons8.com/?size=100&id=39907&format=png&color=000000',
+                                      fit: BoxFit.contain,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ), // tools
+                ],
+              ),
+            ),
           ),
-          Container(
-            padding: const EdgeInsets.all(16.0),
-            decoration: BoxDecoration(
-              color: Colors.grey[100],
-              borderRadius: BorderRadius.circular(10),
-            ),
-            child: const Text(
-              'I am Rilo Prianoko, a 7th-semester student at Gunadarma University. I have a strong interest in Machine Learning, Data Science, and Flutter-based application development. Besides focusing on academics, '
-              'I also have experience working on deep learning projects for customer churn prediction and medical image classification. Currently, I am continuously developing my skills in programming and data technology to achieve a better career in the future.',
-              style: TextStyle(fontSize: 15, color: Colors.black),
-
-              textAlign: TextAlign.justify, // Mengatur text alignment
-            ),
-          ), //desc
-          const SizedBox(height: 16),
-          Container(
-            padding: const EdgeInsets.all(16.0),
-            decoration: BoxDecoration(
-              color: Colors.blue[200],
-              borderRadius: BorderRadius.circular(10),
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text(
-                  'Tools',
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                ),
-                const SizedBox(height: 10), // Spasi antara judul dan icon
-
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    // Icon TensorFlow
-                    Column(
-                      children: [
-                        Container(
-                          width: 30,
-                          height: 30,
-                          child: Image.network(
-                            'https://img.icons8.com/?size=100&id=n3QRpDA7KZ7P&format=png&color=000000',
-                            fit: BoxFit.contain,
-                          ),
-                        ),
-                        Container(
-                          width: 30,
-                          height: 30,
-                          child: Image.network(
-                            'https://img.icons8.com/?size=100&id=13441&format=png&color=000000',
-                          ),
-                        ),
-                        Container(
-                          width: 30,
-                          height: 30,
-                          child: Image.network(
-                            'https://img.icons8.com/?size=100&id=lOqoeP2Zy02f&format=png&color=000000',
-                            fit: BoxFit.contain,
-                          ),
-                        ),
-                      ],
-                    ),
-
-                    // Icon Power BI
-                    Column(
-                      children: [
-                        Container(
-                          width: 30,
-                          height: 30,
-                          child: Image.network(
-                            'https://img.icons8.com/?size=100&id=qYfwpsRXEcpc&format=png&color=000000',
-                            fit: BoxFit.contain,
-                          ),
-                        ),
-                        Container(
-                          width: 30,
-                          height: 30,
-                          child: Image.network(
-                            'https://img.icons8.com/?size=100&id=55251&format=png&color=000000',
-                            fit: BoxFit.contain,
-                          ),
-                        ),
-                        Container(
-                          width: 30,
-                          height: 30,
-                          child: Image.network(
-                            'https://img.icons8.com/?size=100&id=9Kvi1p1F0tUo&format=png&color=000000',
-                            fit: BoxFit.contain,
-                          ),
-                        ),
-                      ],
-                    ),
-
-                    // Icon Jupyter
-                    Column(
-                      children: [
-                        Container(
-                          width: 30,
-                          height: 30,
-                          child: Image.network(
-                            'https://img.icons8.com/?size=100&id=aR9CXyMagKIS&format=png&color=000000',
-                            fit: BoxFit.contain,
-                          ),
-                        ),
-                        Container(
-                          width: 30,
-                          height: 30,
-                          child: Image.network(
-                            'https://img.icons8.com/?size=100&id=SruJhzn0nnLl&format=png&color=000000',
-                            fit: BoxFit.contain,
-                          ),
-                        ),
-                        Container(
-                          width: 30,
-                          height: 30,
-                          child: Image.network(
-                            'https://img.icons8.com/?size=100&id=39907&format=png&color=000000',
-                            fit: BoxFit.contain,
-                          ),
-                        ),
-                      ],
-                    ),
-                    Column(
-                      children: [
-                        Container(
-                          width: 30,
-                          height: 30,
-                          child: Image.network(
-                            'https://img.icons8.com/?size=100&id=117561&format=png&color=000000',
-                            fit: BoxFit.contain,
-                          ),
-                        ),
-                        Container(
-                          width: 30,
-                          height: 30,
-                          child: Image.network(
-                            'https://img.icons8.com/?size=100&id=13406&format=png&color=000000',
-                            fit: BoxFit.contain,
-                          ),
-                        ),
-                        Container(
-                          width: 30,
-                          height: 30,
-                          child: Image.network(
-                            'https://img.icons8.com/?size=100&id=CLvQeiwFpit4&format=png&color=000000',
-                            fit: BoxFit.contain,
-                          ),
-                        ),
-                      ],
-                    ),
-                    Column(
-                      children: [
-                        Container(
-                          width: 30,
-                          height: 30,
-                          child: Image.network(
-                            'https://img.icons8.com/?size=100&id=7I3BjCqe9rjG&format=png&color=000000',
-                            fit: BoxFit.contain,
-                          ),
-                        ),
-                        Container(
-                          width: 30,
-                          height: 30,
-                          child: Image.network(
-                            'https://img.icons8.com/?size=100&id=3tC9EQumUAuq&format=png&color=000000',
-                            fit: BoxFit.contain,
-                          ),
-                        ),
-                        Container(
-                          width: 30,
-                          height: 30,
-                          child: Image.network(
-                            'https://img.icons8.com/?size=100&id=20906&format=png&color=000000',
-                            fit: BoxFit.contain,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ), //tools
         ],
       ),
     );
